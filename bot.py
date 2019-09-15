2c97b7da646ed2124e100d86df764f6404cc82bb
Orkhantri <orkhan_1997@mail.ru>
2019-09-15 12:31:11 +0300

    Поправил код

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import settings
 import ephem
 import time
 
+
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
@@ -18,47 +19,20 @@ def planet(bot,update):
    m='Введите название планеты (например: "Mars"): '
    update.message.reply_text(m)
 
+space = {'Mars': ephem.Mars(time.time()), 'Mercury': ephem.Mercury(time.time()), 
+            'Venus': ephem.Venus(time.time()), 'Jupiter': ephem.Jupiter(time.time()), 
+            'Saturn': ephem.Saturn(time.time()), 'Uranus': ephem.Uranus(time.time()), 
+            'Neptune': ephem.Neptune(time.time()), 'Pluto': ephem.Pluto(time.time()) }
+
 def star(bot,update):
-    k=update.message.text
-    if k=='Mars':
-        mars = ephem.Mars(time.time())
-        const=ephem.constellation(mars)
-        update.message.reply_text(f'Сегодня планета {k} находится в созвездии {const}')
-    elif k=='Mercury':
-        mercury = ephem.Mercury(time.time())
-        const=ephem.constellation(mercury)
-        update.message.reply_text(f'Сегодня планета {k} находится в созвездии {const}')
-    elif k=='Venus':
-        venus = ephem.Venus(time.time())
-        const=ephem.constellation(venus)
-        update.message.reply_text(f'Сегодня планета {k} находится в созвездии {const}')
-    elif k=='Earth':
-        earth = ephem.Earth(time.time())
-        const=ephem.constellation(earth)
-        update.message.reply_text(f'Сегодня планета {k} находится в созвездии {const}')
-    elif k=='Jupiter':
-        jupiter = ephem.Jupiter(time.time())
-        const=ephem.constellation(jupiter)
-        update.message.reply_text(f'Сегодня планета {k} находится в созвездии {const}')
-    elif k=='Saturn':
-        saturn = ephem.Saturn(time.time())
-        const=ephem.constellation(saturn)
-        update.message.reply_text(f'Сегодня планета {k} находится в созвездии {const}')
-    elif k=='Uranus':
-        uranus = ephem.Uranus(time.time())
-        const=ephem.constellation(uranus)
-        update.message.reply_text(f'Сегодня планета {k} находится в созвездии {const}')
-    elif k=='Neptune':
-        neptune = ephem.Neptune(time.time())
-        const=ephem.constellation(neptune)
-        update.message.reply_text(f'Сегодня планета {k} находится в созвездии {const}')
-    elif k=='Pluto':
-        pluto = ephem.Pluto(time.time())
-        const=ephem.constellation(Pluto)
-        update.message.reply_text(f'Сегодня планета {k} находится в созвездии {const}')
+    planet=update.message.text
+    if planet in space:
+        my_planet=space[planet]
+        const=ephem.constellation(my_planet)
+        update.message.reply_text(f'Сегодня планета {planet} находится в созвездии {const}')
     else: 
-        text="Такой планеты не существует"
-        update.message.reply_text(text)
+        update.message.reply_text('такой планеты не существует')
+    
 
 #def talk_to_me(bot,update):
 #    user_text = "Привет,{}! Ты написал: {}".format(update.message.chat.first_name, update.message.text)
